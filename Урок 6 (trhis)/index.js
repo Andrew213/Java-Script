94ca7a93cb28b58ffd2c0de8902606cf47a2d7c2
andrew.wander <andrey.belii2017@yandex.ru>
2020-09-17 21:22:34 +0300

    added 6 lesson (auth)

--- Урок 6 (trhis)/index.js
+++ Урок 6 (trhis)/index.js
@@ -1,9 +1,6 @@
 "use strict";
 
-let input = prompt("Вводи имя и фамилию");
-let x = input.split(" ");
-
-{
+function time() {
   let d = new Date();
   let date = d.getDate();
   let month = d.getMonth() + 1;
@@ -11,19 +8,44 @@ let x = input.split(" ");
   let h = d.getHours();
   let m = d.getMinutes();
   let s = d.getSeconds();
-  window.dateTime =
-    year + "-" + month + "-" + date + " (" + h + ":" + m + ":" + s + ")";
+  return year + "-" + month + "-" + date + " (" + h + ":" + m + ":" + s + ")";
+
 }
 
-function User() {
-  this.name = x[0];
-  this.surname = x[1];
-  this.regDate = dateTime;
-  // this.userInfo = function () {
-  //   return this.name + " " + this.surname + " / " + this.regDate;
-  // };
+
+function UserList() {
+  this.users = [];
+  this.add = oneUser => this.users.push(oneUser);
+  this.getAllUser = () => {
+    return this.users
+  }
 }
 
-function UserList() {}
-let user = new User();
-alert(user.userInfo());
+let useradd = new UserList();
+
+function auth() {
+  let input;
+
+  if (input = prompt("Введите имя и фамилию")) {
+    let arrInput = input.split(' ')
+    if (arrInput[0] === '' || arrInput[1] === '' || arrInput[1] === undefined) {
+      alert('Некорректные данные')
+      return auth()
+    }
+
+    function User() {
+      this.name = arrInput[0];
+      this.surname = arrInput[1];
+      this.regDate = time();
+      this.toString = function () {
+        return 'Имя: ' + this.name + ' Фамилия: ' + this.surname + ' Дата регистрации: ' + this.regDate + '\n'
+      }
+    }
+    let newuser = new User();
+    useradd.add(newuser);
+    auth()
+  } else {
+    alert(useradd.getAllUser().join('').toString())
+  }
+}
+auth()
